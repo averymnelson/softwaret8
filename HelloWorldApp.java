2295ee45d4ff7af02139ac727b0654410cc86f0a
Laine Rainbolt <lar021@uark.edu>
2023-02-06 23:50:46 -0600

    Added code to display splash screen and sql code

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,6 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 class HelloWorldApp {
- public static void main(String[] args) {
- System.out.println("Hello World!"); // Display the string.
- System.out.println("adding changes 2/6/23 2:49 pm");
+  public static void main(String[] args) {
+    System.out.println("Hello World!"); // Display the string.
+    System.out.println("adding changes 2/6/23 2:49 pm");
+  }
+}
+   
+class ShowPicture {
+ public static void main(String args[]) {
+  var frame = new JFrame();
+  var icon = new ImageIcon("logo.jpg");
+  var label = new JLabel(icon);
+  frame.add(label);
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  frame.pack();
+  frame.setVisible(true);
  }
 }
