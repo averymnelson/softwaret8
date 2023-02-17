7e662c479b1ff5ba39bf76b48227b14a423304db
claire <crdildy@uark.edu>
2023-02-16 22:08:54 -0600

    Added a start button

--- playerEntry.java
+++ playerEntry.java
@@ -1,108 +1,77 @@
-//import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
-//import java.awt.event.MouseEvent;
-//import javax.swing.GroupLayout;
-//import java.awt.GridLayout;
 import javax.swing.JFrame;
-//import javax.security.auth.kerberos.KerberosCredMessage;
-//import javax.swing.JPanel;
-import javax.swing.*;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-//import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+// import javax.swing.JTextArea;
+// import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.JTable;
-
+import javax.swing.*; //to cover every import for now, can delete later
 // claire, gracie
 
 //Class playerEntry : Creates tables and allows user to enter text
 public class playerEntry extends JFrame implements ActionListener {
 
     //Instance variables/constants
-    protected JTextField textField;
-    protected JTextArea textArea;
+    // protected JTextField textField;
+    // protected JTextArea textArea;
     //private final static String newline = "\n";
     public static int ID;
     public static String fName, lName, codeName; 
     
     //Constructor
     public playerEntry(){
-        //New GridLayout object: one component per rectangle (rows, columns)
-        //GridLayout grid = new GridLayout(1, 0);
-        //Column labels
-
-        // textField = new JTextField(10);
-        // textField.addActionListener(this);
-        // textArea = new JTextArea(4, 10);
-        //Make the text box editable for player entry/changes to player names
-        // textArea.setEditable(true);
-        // JTable table = new JTable(10, 2);
-        // table.setBounds(30,40,200,300);
-        // table.setShowGrid(true);
-        //Add Components to this panel.
-        
-        // JFrame frame = new JFrame("EntryScreenDemo");
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.setSize(600, 600);
-        // frame.setVisible(true);
-        // JScrollPane scrollPane = new JScrollPane(table);
-        // //Add the scroll pane to this panel.
-        // add(scrollPane);
-
-        // textField = new JTextField(10);
-        // textField.addActionListener(this);
-        // textArea = new JTextArea(4, 10);
-        // textArea.setEditable(true);
+      
     }
+
     //Create a table for player entry
     public void createGUI () {
-
-        //JTable table = new JTable(10, 2);
+        //New Frame and panel objects 
         JPanel panel = new JPanel();
         JFrame frame = new JFrame("Entry Terminal");
 
         panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Edit Current Game", TitledBorder.CENTER, TitledBorder.TOP));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Team 1"));
-        // frame.setSize(600, 600);
-        // frame.setVisible(true);
-        //JScrollPane scrollPane = new JScrollPane(table);
-        //Add the scroll pane to this panel.
-        // add(scrollPane);
-
+        
+        //Table for player entry 
         String players[][] = new String[20][4];   
         String [] columnName = {"ID", "Codename","ID", "Codename" };
         JTable table = new JTable(players,columnName);
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        frame.add(panel);
+        frame.setSize(1000, 636);
         
+        //can switch to using textField if the table begins to cause trouble later
         // textArea.setEditable(true);
         // textField = new JTextField(10);
         // textField.addActionListener(this);
         // textArea = new JTextArea(4, 10);
-        JScrollPane scrollPane = new JScrollPane(table);
+        //frame.getContentPane().add(new JButton("Click")); //frame.add(new JButton("Click"));//adds to the whole screen
         
-        panel.add(scrollPane, BorderLayout.CENTER);
-        frame.add(panel);
-        frame.setSize(1000, 636);
+        //Able to add an icon to a button! will do later... - Claire
+        //Glitch where the player screen pops up for a second?
+        JButton bStart = new JButton("Start");
+        panel.add(bStart);
         frame.setVisible(true);
+        
+        //Formatting the table
         //table.setBounds(30,40,200,300);
         // table.setShowGrid(true);
-        //add(scrollPane);
         table.setGridColor(Color.gray);
         table.setBackground(Color.pink);
         table.setRowSelectionAllowed(false);
         table.setSelectionBackground(Color.pink);
         table.setSelectionForeground(Color.pink);
-        
     }
-
-    // }
     
     //Override ActionListener methods
     // @Override
+    //Can add actions for the buttons on screen later
     public void actionPerformed(ActionEvent e) {
         // String text = textField.getText();
         // textArea.append(text + newline);
@@ -112,8 +81,4 @@ public class playerEntry extends JFrame implements ActionListener {
     // INSERT INTO player (id, first_name, last_name, codename)
     // VALUES (1, 'Jim', 'Strother', 'Opus');
     }
-
-    // private static void run() {
-    //     createAndShowGUI();
-    // }
 }
