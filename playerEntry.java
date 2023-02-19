12e1067f4c95d7e0c531db54ab5227e6252b99e0
Laine Rainbolt <lar021@uark.edu>
2023-02-19 16:22:10 -0600

    Code Clean up

--- playerEntry.java
+++ playerEntry.java
@@ -1,19 +1,10 @@
-//import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
-//import java.awt.event.MouseEvent;
-//import javax.swing.GroupLayout;
-//import java.awt.GridLayout;
 import javax.swing.JFrame;
-//import javax.security.auth.kerberos.KerberosCredMessage;
-//import javax.swing.JPanel;
 import javax.swing.*;
 import javax.swing.JScrollPane;
-//import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.JTable;
 
@@ -23,41 +14,12 @@ import javax.swing.JTable;
 public class playerEntry extends JFrame implements ActionListener {
 
     //Instance variables/constants
-    protected JTextField textField;
-    protected JTextArea textArea;
-    //private final static String newline = "\n";
     public static int ID;
     public static String fName, lName, codeName; 
     
     //Constructor
-    public playerEntry(){
-        //New GridLayout object: one component per rectangle (rows, columns)
-        //GridLayout grid = new GridLayout(1, 0);
-        //Column labels
+    public playerEntry(){}
 
-        // textField = new JTextField(10);
-        // textField.addActionListener(this);
-        // textArea = new JTextArea(4, 10);
-        //Make the text box editable for player entry/changes to player names
-        // textArea.setEditable(true);
-        // JTable table = new JTable(10, 2);
-        // table.setBounds(30,40,200,300);
-        // table.setShowGrid(true);
-        //Add Components to this panel.
-        
-        // JFrame frame = new JFrame("EntryScreenDemo");
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.setSize(600, 600);
-        // frame.setVisible(true);
-        // JScrollPane scrollPane = new JScrollPane(table);
-        // //Add the scroll pane to this panel.
-        // add(scrollPane);
-
-        // textField = new JTextField(10);
-        // textField.addActionListener(this);
-        // textArea = new JTextArea(4, 10);
-        // textArea.setEditable(true);
-    }
     //Create a table for player entry
     public void createGUI () {
 
@@ -67,40 +29,25 @@ public class playerEntry extends JFrame implements ActionListener {
 
         panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Edit Current Game", TitledBorder.CENTER, TitledBorder.TOP));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Team 1"));
-        // frame.setSize(600, 600);
-        // frame.setVisible(true);
-        //JScrollPane scrollPane = new JScrollPane(table);
-        //Add the scroll pane to this panel.
-        // add(scrollPane);
 
         String players[][] = new String[20][4];   
         String [] columnName = {"ID", "Codename","ID", "Codename" };
         JTable table = new JTable(players,columnName);
-        
-        // textArea.setEditable(true);
-        // textField = new JTextField(10);
-        // textField.addActionListener(this);
-        // textArea = new JTextArea(4, 10);
         JScrollPane scrollPane = new JScrollPane(table);
-        
+        JButton bStart = new JButton("Start Game");
+
         panel.add(scrollPane, BorderLayout.CENTER);
         frame.add(panel);
         frame.setSize(1000, 636);
+        panel.add(bStart);
         frame.setVisible(true);
-        //table.setBounds(30,40,200,300);
-        // table.setShowGrid(true);
-        //add(scrollPane);
+        
         table.setGridColor(Color.gray);
         table.setBackground(Color.pink);
         table.setRowSelectionAllowed(false);
         table.setSelectionBackground(Color.pink);
         table.setSelectionForeground(Color.pink);
-        
     }
-
-    // }
     
     //Override ActionListener methods
     // @Override
@@ -112,18 +59,5 @@ public class playerEntry extends JFrame implements ActionListener {
         // // --Place first record into table
     // INSERT INTO player (id, first_name, last_name, codename)
     // VALUES (1, 'Jim', 'Strother', 'Opus');
-    }
-
-    // private static void run() {
-    //     createAndShowGUI();
-    // }
-
-    // Main
-    public static void main(String[] args){
-        System.out.println("Hello");
-        playerEntry entryScreen = new playerEntry();
-        createGUI();
-        //createAndShowGUI();
-        //run();
     }
 }
