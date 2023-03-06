88acf1b4655efdb675cf71d0793a98c178f30c69
Avery Nelson <123122545+averymnelson@users.noreply.github.com>
2023-03-06 11:26:14 -0600

    fixing play action, making panels and frames instead of tables

--- src/main/java/test/playActionDisplay.java
+++ src/main/java/test/playActionDisplay.java
@@ -1,10 +1,13 @@
 package test;
 
-import java.awt.*;
+//import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 
-public class playActionDisplay extends JFrame {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class playActionDisplay extends JPanel {
 
     public static int ID;
     public static String fName, lName, codeName;
@@ -15,47 +18,36 @@ public class playActionDisplay extends JFrame {
     }
 
     public void createGUI() {
+        JPanel mainPanel, subPanel1, subPanel2, subPanel3, subPanel4;
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        frame.add(panel);
+        mainPanel = new JPanel();
+        frame.add(mainPanel);
         frame.setSize(1000, 300);
         frame.setFocusable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        JPanel mainPanel, subPanel1, subPanel2, subPanel3, subPanel4;
-        panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Playing Current Game",
+        mainPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Playing Current Game",
             TitledBorder.CENTER, TitledBorder.TOP));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainPanel = new JPanel();
         mainPanel.setBorder(BorderFactory.createTitledBorder("Edit Current Game"));
-        //mainPanel.setLayout(new GridBagLayout());
-        //GridBagConstraints c = new GridBagConstraints();
 
-        // Constructing JPanel 1 and 2 with GridLayout of 1 row and 1 column
         subPanel1 = new JPanel();
         subPanel1.setBorder(BorderFactory.createTitledBorder("Team 1"));
-        //subPanel1.setLayout(new GridLayout(1, 1));
+
         subPanel2 = new JPanel();
-        subPanel2.setBorder(BorderFactory.createTitledBorder("Team 2"));
-        //subPanel2.setLayout(new GridLayout(1, 1));
+        subPanel2.setBorder(BorderFactory.createTitledBorder("Team 1 Log"));
+
         subPanel3 = new JPanel();
-        subPanel3.setBorder(BorderFactory.createTitledBorder("Team 1"));
-        //subPanel3.setLayout(new GridLayout(1, 1));
+        subPanel3.setBorder(BorderFactory.createTitledBorder("Team 2"));
+
         subPanel4 = new JPanel();
-        subPanel4.setBorder(BorderFactory.createTitledBorder("Team 2"));
-        //subPanel4.setLayout(new GridLayout(1, 1));
+        subPanel4.setBorder(BorderFactory.createTitledBorder("Team 2 Log"));
 
-        String[] columnName = { "ID", "Codename" };
-        JTable table1 = new JTable(team1Players, columnName);
-        JTable table2 = new JTable(team2Players, columnName);
-        JTable table3 = new JTable(team1Players, columnName);
-        JTable table4 = new JTable(team2Players, columnName);
-        JScrollPane scrollPane1 = new JScrollPane(table1);
-        JScrollPane scrollPane2 = new JScrollPane(table2);
+        JScrollPane scrollPane1 = new JScrollPane();
+        JScrollPane scrollPane2 = new JScrollPane();
 
-        // Adding JPanel 1 and 2 to main JPanel
         subPanel1.add(scrollPane1);
         subPanel2.add(scrollPane2);
         mainPanel.add(subPanel1);
@@ -66,49 +58,44 @@ public class playActionDisplay extends JFrame {
         frame.add(mainPanel);
         frame.setSize(1000, 636);
         frame.setVisible(true);
-        table1.setRowHeight(26);
-        //table1.setGridColor(Color.gray);
-        table1.setBackground(Color.pink);
-        table1.setRowSelectionAllowed(false);
-
-        table2.setRowHeight(26);
-        //table2.setGridColor(Color.gray);
-        table2.setBackground(Color.pink);
-        table2.setRowSelectionAllowed(false);
-
-        table3.setRowHeight(26);
-        //table3.setGridColor(Color.gray);
-        table3.setBackground(Color.pink);
-        table3.setRowSelectionAllowed(false);
-
-        table4.setRowHeight(26);
-        //table4.setGridColor(Color.gray);
-        table4.setBackground(Color.pink);
-        table4.setRowSelectionAllowed(false);
+        //repaint is currently doing nothing
+        subPanel1.repaint();
 
         GroupLayout layout = new GroupLayout(mainPanel);
         mainPanel.setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
         GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
-        hGroup.addGroup(layout.createParallelGroup().addComponent(table1).addComponent(table2));
-        hGroup.addGroup(layout.createParallelGroup().addComponent(table3).addComponent(table4));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(subPanel1).addComponent(subPanel2));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(subPanel3).addComponent(subPanel4));
         layout.setHorizontalGroup(hGroup);
         GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
-        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(table1)
-            .addComponent(table3));
-        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(table2)
-            .addComponent(table4));
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(subPanel1)
+            .addComponent(subPanel3));
+        vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(subPanel2)
+            .addComponent(subPanel4));
         layout.setVerticalGroup(vGroup);
     }
-    //take off grids
-    //make it stop loading a new window all the time
     //adjust bottom two tables to scroll
 
-    //figure out why we have to click
     //adjust top two to pull from database list players
     //add points field should stay 0 though
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); 
+        g.drawString("string literal or a string variable", 0,10);
+        g.setColor(new Color(227, 115, 131));
+        g.fillRect(0,0,this.getWidth(), this.getHeight());
 
-
-
-}
+        //in game.java from mario (in the run function):
+        //view.repaint();
+        //in view.java from mario:
+        // public void paintComponent(Graphics g){
+        //     g.setColor(new Color(128,255,255));
+        //     scrollPos=model.mario.x-100;
+        //     g.fillRect(0,0,this.getWidth(), this.getHeight());
+        //     g.setColor(Color.gray);
+        //     g.drawLine(0, 596, 2000, 596);
+        //     for(int i=0; i<model.sprites.size(); i++){
+        //         model.sprites.get(i).draw(g, scrollPos);
+        }
+    }
